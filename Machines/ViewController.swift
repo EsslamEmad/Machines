5b0d2230f2bd3627a1de81df28848f7e592bcc45
esslam emad <esslam69@gmail.com>
2018-10-24 14:14:19 +0200

    Initial Commit

---
+++ Machines/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machines
+//
+//  Created by Esslam Emad on 24/10/18.
+//  Copyright © 2018 Alyom Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
